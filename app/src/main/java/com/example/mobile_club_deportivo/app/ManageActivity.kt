39745c0666cf8df03d16ed751e8aa62a34232f69
BrianSabio <Brian232245@gmail.com>
2026-05-10 10:34:12 -0400

    feat: Implementar layout Registrar Cliente y Gestionar

---
+++ app/src/main/java/com/example/mobile_club_deportivo/app/ManageActivity.kt
@@ -0,0 +1,14 @@
+package com.example.mobile_club_deportivo.app
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class ManageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_manage)
+    }
+}
